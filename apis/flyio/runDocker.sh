9db8c25cdcec6c15b0083fb97e9f2a63fb49a6a9
Julien Lengrand-Lambert <julien@lengrand.fr>
2024-03-07 16:17:20 +0100

    Adding flyio APIs

--- apis/flyio/runDocker.sh
+++ apis/flyio/runDocker.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # The file was found here : https://community.fly.io/t/fly-machines-rest-api-openapi-specification/8207/8
+# The specs are available there too https://docs.machines.dev/
 
 docker run --rm \
   -v ${PWD}:/local julienlengrand/jetbrains-openapi-generator generate \
+  --additional-properties=customHeaders="Authorization: Bearer {{bearerToken}}" \
   -i /local/doc.json \
   -g jetbrains-http-client \
   -o /local
